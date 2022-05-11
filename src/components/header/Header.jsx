6c24df635ed61e615775e82f3c99865023b4d812
Tyler Gandy <tylerbgandy@gmail.com>
2022-05-11 19:54:25 -0400

    added icons to homepage

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./header.css";
 import Code from "./Code";
+import Tyler from "../../assets/tylerMain.jpg";
+import SocialMedia from "./SocialMedia";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I am</h5>
+        <h5>Hey There, my name is</h5>
         <h1>Tyler B. Gandy</h1>
-        <h5 className="text-light">Fullstck Javascript Developer</h5>
+        <h5 className="text-light">I am a Fullstck Javascript Developer</h5>
         <Code />
+        <SocialMedia />
+        <div className="me">
+          <img src={Tyler} alt="Tyler B. Gandy"></img>
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
---
+++ src/components/header/SocialMedia.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub, FaFacebook } from "react-icons/fa";
+
+const SocialMedia = () => {
+  return (
+    <div classNAme="social__media">
+      <a
+        href="https://www.linkedin.com/in/tyler-gandy-a90a41225/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/TyGBenjamin" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://www.facebook.com/" target="_blank">
+        <FaFacebook />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMedia;
