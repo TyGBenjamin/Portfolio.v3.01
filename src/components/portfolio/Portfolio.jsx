bc4ac946cadd65e267b2f57e58ff458705d12daa
Tyler Gandy <tylerbgandy@gmail.com>
2022-05-13 17:53:30 -0400

    almost complete

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,60 @@
 import React from "react";
 import "./portfolio.css";
+import Blue from "../../assets/BlueSky1.jpg";
+import ProjectT from "../../assets/projectThreeOne.jpg";
+import Jate from "../../assets/jateTextEdit.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: Blue,
+    title: " Socail Media Macth Maker",
+    github: "https://github.com/Lischus/Blue-Sky-Farmers",
+    beta: "https://radiant-caverns-76977.herokuapp.com/",
+  },
+  {
+    id: 2,
+    image: ProjectT,
+    title: " Sports Betting App",
+    github: "https://github.com/TyGBenjamin/Chartreuse_Gamblers",
+    beta: "https://chartreuse-gamblers-app.herokuapp.com/",
+  },
+  {
+    id: 3,
+    image: Jate,
+    title: " Not Another NotePad",
+    github: "https://github.com/TyGBenjamin/Not_Another_NotePad",
+    beta: "https://not-another-notepad.herokuapp.com/",
+  },
+];
 
 const Portfolio = () => {
-  return <section id="portfolio"> Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5> Recent Projects and Repositories</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, beta }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}></img>
+              </div>
+              <h3> {title}</h3>
+              <div className="portfolio__item-cta">
+                <a className="btn" href={github} target="blank">
+                  GITHUB
+                </a>
+                <a className="btn btn-primary" href={beta} target="blank">
+                  DEPLOYED
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,179 @@
 import React from "react";
 import "./services.css";
+import { BiCheckboxSquare } from "react-icons/bi";
 
 const Services = () => {
-  return <section id="services"> Services</section>;
+  return (
+    <section id="services">
+      <h5>What I can do </h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI  */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEBDEV  */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Creative Content</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+            <li>
+              <BiCheckboxSquare className="service__lict-icon" />
+              <p>
+                {" "}
+                Hello I will fill in this apce after completing the layout of my
+                portfolio
+              </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF CC  */}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
